b79512fe83e11b94830e424761b2c791a13132c9
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-02-02 19:20:46 +0530

    Update copy.py

--- plugins/copy.py
+++ plugins/copy.py
@@ -1,1 +1,13 @@
+from info import FROM, TO
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
 
+@Client.on_message(filters.chat(FROM) & (filters.document | filters.video)) #add more filters if you want.
+async def copy(c, m):
+    try:
+        if m.media and not (m.video_note or m.sticker):
+            await m.copy(TO, caption = m.caption if m.caption else None)
+        else:
+            await m.copy(TO)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
